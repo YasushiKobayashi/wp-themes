5db694f336c0bf53d33629fdcf7b4728e5e171de
Kobayashi(attela) <kobayashi@attela.jp>
2016-11-06 01:34:16 +0900

    fix

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <dl>
             <dt>
-                <?php if ( has_post_thumbnail() ): // サムネイルを持っているときの処理 ?>
-                    <?php the_post_thumbnail(); ?>
-                <?php else: // サムネイルを持っていないときの処理 ?>
+                <?php if (has_post_thumbnail()):
+                    the_post_thumbnail();
+                else: // サムネイルを持っていないときの処理 ?>
                     <img src="<?php echo get_template_directory_uri(); ?>/images/no_image.png" alt="no image" title="no image" width="100" height="100" />
                 <?php endif; ?>
             </dt>
